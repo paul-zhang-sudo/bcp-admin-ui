65f2d8a84ac5d3a633919e583be08d2775d1ad44
费锡军 <feixijun@bizsoftinfo.com>
2021-09-06 15:31:04 +0800

    解决错误的token导致系统报错的问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -16,12 +16,11 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     // do something before request is sent
-
     if (store.getters.token) {
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['b-token'] = store.getters.token
+      config.headers['b-token'] = encodeURIComponent(store.getters.token)
     }
     return config
   },

427e87748a6d503efad85286f0bfc7359c82cd98
fish <java_fish@126.com>
2021-12-16 16:39:58 +0800

    身份认证失败，不跳转

--- src/views/authLogin/index.vue
+++ src/views/authLogin/index.vue
@@ -2,8 +2,8 @@
   <div class="app-container">
      <div class="background" align="center">
       <img :src="loadSrc" alt="" />
-      <span style="display:inherit;font-size: 22px;margin-top: 30px">身份认证中</span>
-      <span style="display:inherit;font-size: 15px;margin-top: 10px">请勿关闭页面，稍后会进行跳转</span>
+      <span style="display:inherit;font-size: 22px;margin-top: 30px">{{msg1}}</span>
+      <span style="display:inherit;font-size: 15px;margin-top: 10px">{{msg2}}</span>
     </div>
   </div>
 </template>
@@ -13,7 +13,9 @@ import * as api from '@/api/ssoLogin'
 export default {
   data() {
     return {
-      loadSrc: require('@/img/loading.gif')
+      loadSrc: require('@/img/loading.gif'),
+      msg1: '身份认证中',
+      msg2: '请勿关闭页面，稍后会进行跳转'
     }
   },
   created() {
@@ -25,15 +27,18 @@ export default {
         "tenant":this.$route.query.tenant,
         "code":this.$route.query.code
       }
-      console.log('开始调用登录接口，参数:'+params)
       api.ssoLogin(params).then(res => {
-        console.log('登录接口调用完毕，返回结果:'+res)
         console.log(res)
-        this.$store.dispatch('user/token', res.msg).then(rs => {
-          console.log('cookie保存完毕,准备进入首页,rs:'+rs)
-          this.$router.push({ path: this.redirect || '/' })
-          this.loading = false
-        })
+        if( res.code===500 ){
+          this.msg1 = '身份认证失败'
+          this.msg2 = ''
+        }else{
+          this.$store.dispatch('user/token', res.msg).then(rs => {
+            console.log('cookie保存完毕,准备进入首页,rs:'+rs)
+            this.$router.push({ path: this.redirect || '/' })
+            this.loading = false
+          })
+        }
       }).catch(e => {
         console.log('单点登录失败,错误信息:'+e)
       })

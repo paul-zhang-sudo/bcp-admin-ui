d65c26dd2371210d795bb9732a6ca80365fa7c6d
bilibili_ming <768261620@qq.com>
2022-04-19 18:07:02 +0800

    新增集成场景需求

--- src/views/CaseIR/index.vue
+++ src/views/CaseIR/index.vue
@@ -33,15 +33,27 @@
       </el-table-column>
 
       <el-table-column
-        prop="link"
         label="文档链接"
         width="500px"
         align="center">
         <template slot-scope="scope"
         align="center">
-          <el-button @click="handleClick(scope.row)" type="text" size="small">查看详情</el-button>
+          <el-button @click="handleClick(scope.row.link)" type="text" size="small">需求文档</el-button>
+          <el-button @click="handleClick(scope.row.guide)" type="text" size="small">测试指引</el-button>
         </template>
       </el-table-column>
+
+      <!-- <el-table-column
+        prop="guide"
+        label="文档链接"
+        width="500px"
+        align="center">
+        <template slot-scope="scope"
+        align="center">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">测试指引</el-button>
+        </template>
+      </el-table-column> -->
+
       </el-table-column>
     </el-table>
   </div>
@@ -56,68 +68,103 @@ export default {
           type: "SRM",
           business: "甄云",
           system: "K3",
-          link: "https://www.kdocs.cn/l/cfE8hRgT2gWC"
+          link: "https://www.kdocs.cn/l/cfE8hRgT2gWC",
+          guide: "https://kdocs.cn/l/cfuCzBANMPG2"
         },
           {
           type: "SRM",
           business: "甄云",
           system: "K3C",
-          link: "https://www.kdocs.cn/l/cblSU13JgYaE"
+          link: "https://www.kdocs.cn/l/cblSU13JgYaE",
+          guide: "https://kdocs.cn/l/cg8BSZ4W89gB"
         },
          {
           type: "SRM",
           business: "甄云",
           system: "NCC",
-          link: "https://www.kdocs.cn/l/cfWGO5Jrjvje"
+          link: "https://www.kdocs.cn/l/cfWGO5Jrjvje",
+          guide: "https://kdocs.cn/l/cltK4nt9jt7b"
         },
         {
           type: "SRM",
           business: "甄云",
           system: "U8",
-          link: "https://docs.qq.com/sheet/DT1lhTmNReHplbVZq?tab=BB08J3"
+          link: "https://docs.qq.com/sheet/DT1lhTmNReHplbVZq?tab=BB08J3",
+          guide: "https://kdocs.cn/l/ci6aKuveXdaA"
+        },
+        {
+          type: "SRM",
+          business: "甄云",
+          system: "SAP ECC",
+          link: "https://kdocs.cn/l/clBWdud4uP4c",
+          guide: "https://kdocs.cn/l/cvjtTcCsJFV5"
         },
         
+        {
+          type: "SRM",
+          business: "甄云",
+          system: "SAP HANA S4/C",
+          link: "https://kdocs.cn/l/cpH7MPIa8pz4",
+          guide: "https://kdocs.cn/l/curIUb2PCvc2"
+        },
+          {
+          type: "SRM",
+          business: "甄云",
+          system: "SAP B1",
+          link: "https://kdocs.cn/l/ccOVZj0M6ft6",
+          guide: "https://kdocs.cn/l/cnxqQghpoGBj"
+        },
+          
+        
+
         {
           type: "MES",
           business: "黑湖",
           system: "K3C",
-          link: "https://www.kdocs.cn/l/ceY9QScUrUc5"
+          link: "https://www.kdocs.cn/l/ceY9QScUrUc5",
+          guide: "https://kdocs.cn/l/ctWTm7Y6gL6X"
         },
         {
           type: "MES",
           business: "语桢",
           system: "优时",
-          link: "https://kdocs.cn/l/cl5aF6JjF0LS"
+          link: "https://kdocs.cn/l/cl5aF6JjF0LS",
+          guide: "https://kdocs.cn/l/cf8nzjwzVspo"
         },
         {
           type: "MES",
           business: "语桢",
           system: "易飞",
-          link: "https://kdocs.cn/l/ceofBYQioRLZ"
+          link: "https://kdocs.cn/l/ceofBYQioRLZ",
+          guide: "https://kdocs.cn/l/cv1FBCJzXAeN"
         },
         {
           type: "MES",
           business: "鼎捷",
           system: "NC",
-          link: "https://kdocs.cn/l/cdmVewGi27d0"
+          link: "https://kdocs.cn/l/cdmVewGi27d0",
+          guide: "https://kdocs.cn/l/cqmLcetxU9uu"
         },
         {
           type: "MES",
           business: "甄一",
           system: "SAP",
-          link: "https://kdocs.cn/l/caCpgmILVRsW"
+          link: "https://kdocs.cn/l/caCpgmILVRsW",
+          guide: "https://kdocs.cn/l/cdehWkm6W4Eq"
         },
          {
           type: "MES",
           business: "智引",
           system: "U9",
-          link: "https://kdocs.cn/l/cqtYZyIz7dPw"
+          link: "https://kdocs.cn/l/cqtYZyIz7dPw",
+          guide: "https://kdocs.cn/l/cpFSvlpSORKP"
         },
         {
           type: "MES",
           business: "中科晟达",
           system: "U8",
-          link: "https://kdocs.cn/l/cbvMhiPrDJEU"
+          link: "https://kdocs.cn/l/cbvMhiPrDJEU",
+          guide: "https://kdocs.cn/l/clmvWL9q4Dww"
         },
 
 
@@ -125,42 +172,87 @@ export default {
           type: "OA",
           business: "泛微",
           system: "SAP",
-          link: "https://www.kdocs.cn/l/coFuhJcQdrvD"
+          link: "https://www.kdocs.cn/l/coFuhJcQdrvD",
+          guide: "https://kdocs.cn/l/cj1gfJEx8as7"
+        },
+         {
+          type: "OA",
+          business: "泛微",
+          system: "K3C",
+          link: "https://kdocs.cn/l/cqCPBTmgwWmp",
+          guide: "https://kdocs.cn/l/cbLV5y5uhSlE"
         },
 
         {
           type: "CRM",
           business: "纷享销客",
           system: "K3C",
-          link: "https://www.kdocs.cn/l/cu052Bc7ZzLh"
+          link: "https://www.kdocs.cn/l/cu052Bc7ZzLh",
+          guide: "https://kdocs.cn/l/ckPIFe8EuLgg"
         },
+
+        {
+          type: "PLM",
+          business: "数码大方",
+          system: "U8",
+          link: "https://kdocs.cn/l/cvW2V4maqjhj",
+          guide: "https://kdocs.cn/l/cqDNTmtaOUV8"
+        },
+        {
+          type: "PLM",
+          business: "数码大方",
+          system: "K3C",
+          link: "https://kdocs.cn/l/ca1atCwRPcHw",
+          guide: "https://kdocs.cn/l/cdbpzmC5gFjw"
+        },
+        {
+          type: "PLM",
+          business: "山东华天",
+          system: "SAP ECC",
+          link: "https://kdocs.cn/l/cp9TZObuYe5E",
+          guide: "https://kdocs.cn/l/cdyY2czu5P6x"
+        },
+
+        {
+          type: "跨境电商",
+          business: "赛盒",
+          system: "T+",
+          link: "https://kdocs.cn/l/csfwIZzDqser",
+          guide: "https://kdocs.cn/l/cdkpCReL6JbK"
+        },
+
+        
 
         {
           type: "IoT",
           business: "盛源成",
           system: "SAP",
-          link: "https://kdocs.cn/l/clzJwEyaHOMd"
+          link: "https://kdocs.cn/l/clzJwEyaHOMd",
+          guide: "https://kdocs.cn/l/ciKnRp3wqT7C"
         },
 
         {
           type: "数字工厂",
           business: "华为",
           system: "优时",
-          link: "https://kdocs.cn/l/cl5aF6JjF0LS"
+          link: "https://kdocs.cn/l/cl5aF6JjF0LS",
+          guide: "https://kdocs.cn/l/ccn03HQve8Em"
         },
 
         {
           type: "BI",
           business: "永洪",
           system: "SAP",
-          link: "https://docs.qq.com/sheet/DVWNrVnpnRGxBcEdS?tab=BB08J2"
+          link: "https://docs.qq.com/sheet/DVWNrVnpnRGxBcEdS?tab=BB08J2",
+          guide: "https://kdocs.cn/l/cqjKGQFGvNxE"
         },
 
         {
           type: "AppCube",
           business: "华为",
           system: "SAP",
-          link: "https://kdocs.cn/l/cqjvAcCgRAhG"
+          link: "https://kdocs.cn/l/cqjvAcCgRAhG",
+          guide: "https://kdocs.cn/l/ciiZhpfLo9iC"
         },
         
       ],
@@ -175,7 +267,7 @@ export default {
     handleClick(row) {
       var el = document.createElement("a");
       document.body.appendChild(el);
-      el.href = row.link; //url 
+      el.href = row; //url 
       el.target = '_new'; //指定在新窗口打开
       el.click();
       document.body.removeChild(el);

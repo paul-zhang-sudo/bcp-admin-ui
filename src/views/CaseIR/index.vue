4af3d95b311157068e57e528460f828a3de3750e
wangzihao@bizsoftinfo.com <WZH1314ai>
2023-01-18 09:48:30 +0800

    知识引擎-集成场景模板 更改数据

--- src/views/CaseIR/index.vue
+++ src/views/CaseIR/index.vue
@@ -22,7 +22,7 @@
         label="业务系统厂商"
         width="300px"
         align="center">
-        
+
       </el-table-column>
 
       <el-table-column
@@ -39,7 +39,7 @@
         <template slot-scope="scope"
         align="center">
           <el-button @click="handleClick(scope.row.link)" type="text" size="small">场景模板</el-button>
-          <el-button @click="handleClick(scope.row.guide)" type="text" size="small">联调指引</el-button>
+          <!--<el-button @click="handleClick(scope.row.guide)" type="text" size="small">联调指引</el-button>-->
         </template>
       </el-table-column>
       </el-table-column>
@@ -53,222 +53,341 @@ export default {
     return {
       tableData: [
         {
-          type: "SRM",
-          business: "甄云",
-          system: "K3",
-          link: "https://www.kdocs.cn/l/cfE8hRgT2gWC",
-          guide: "https://kdocs.cn/l/cfuCzBANMPG2",
+          "type": "SRM",
+          "business": "甄云",
+          "system": "金蝶K3",
+          "link": "https://www.kdocs.cn/l/cfE8hRgT2gWC"
         },
         {
-          type: "SRM",
-          business: "甄云",
-          system: "K3C",
-          link: "https://www.kdocs.cn/l/cblSU13JgYaE",
-          guide: "https://kdocs.cn/l/cg8BSZ4W89gB",
+          "type": "SRM",
+          "business": "甄云",
+          "system": "金蝶K3C",
+          "link": "https://docs.qq.com/sheet/DY09HbXJObkh5eWNP?tab=e4ymbr"
         },
         {
-          type: "SRM",
-          business: "甄云",
-          system: "NCC",
-          link: "https://www.kdocs.cn/l/cfWGO5Jrjvje",
-          guide: "https://kdocs.cn/l/cltK4nt9jt7b",
+          "type": "SRM",
+          "business": "甄云",
+          "system": "鼎捷E10",
+          "link": "https://docs.qq.com/sheet/DRWpIa0VXalpQRFFF?tab=fysejp"
         },
         {
-          type: "SRM",
-          business: "甄云",
-          system: "U8",
-          link: "https://docs.qq.com/sheet/DT1lhTmNReHplbVZq?tab=BB08J3",
-          guide: "https://kdocs.cn/l/ci6aKuveXdaA",
+          "type": "SRM",
+          "business": "甄云",
+          "system": "用友NCC",
+          "link": "https://www.kdocs.cn/l/cfWGO5Jrjvje"
         },
         {
-          type: "SRM",
-          business: "甄云",
-          system: "SAP ECC",
-          link: "https://kdocs.cn/l/clBWdud4uP4c",
-          guide: "https://kdocs.cn/l/cvjtTcCsJFV5",
+          "type": "SRM",
+          "business": "甄云",
+          "system": "用友U8",
+          "link": "https://docs.qq.com/sheet/DT1lhTmNReHplbVZq?tab=BB08J3"
         },
         {
-          type: "SRM",
-          business: "甄云",
-          system: "SAP HANA S4/C",
-          link: "https://kdocs.cn/l/cpH7MPIa8pz4",
-          guide: "https://kdocs.cn/l/curIUb2PCvc2",
+          "type": "SRM",
+          "business": "甄云",
+          "system": "SAP ECC",
+          "link": "https://kdocs.cn/l/clBWdud4uP4c"
         },
         {
-          type: "SRM",
-          business: "甄云",
-          system: "SAP B1",
-          link: "https://kdocs.cn/l/ccOVZj0M6ft6",
-          guide: "https://kdocs.cn/l/cnxqQghpoGBj",
+          "type": "SRM",
+          "business": "甄云",
+          "system": "SAP HANA S4/C",
+          "link": "https://kdocs.cn/l/cpH7MPIa8pz4"
         },
         {
-          type: "MES",
-          business: "黑湖",
-          system: "K3C",
-          link: "https://www.kdocs.cn/l/ceY9QScUrUc5",
-          guide: "https://kdocs.cn/l/ctWTm7Y6gL6X",
+          "type": "SRM",
+          "business": "甄云",
+          "system": "SAP B1",
+          "link": "https://kdocs.cn/l/ccOVZj0M6ft6"
         },
         {
-          type: "MES",
-          business: "语祯",
-          system: "优时",
-          link: "https://kdocs.cn/l/cl5aF6JjF0LS",
-          guide: "https://kdocs.cn/l/cf8nzjwzVspo",
+          "type": "MES",
+          "business": "黑湖",
+          "system": "金蝶K3C",
+          "link": "https://www.kdocs.cn/l/ceY9QScUrUc5"
         },
         {
-          type: "MES",
-          business: "语祯",
-          system: "易飞",
-          link: "https://kdocs.cn/l/ceofBYQioRLZ",
-          guide: "https://kdocs.cn/l/cv1FBCJzXAeN",
+          "type": "MES",
+          "business": "黑湖",
+          "system": "用友U8",
+          "link": "https://www.kdocs.cn/l/csUEgxhocTnr"
         },
         {
-          type: "MES",
-          business: "鼎捷",
-          system: "NC",
-          link: "https://kdocs.cn/l/cdmVewGi27d0",
-          guide: "https://kdocs.cn/l/cqmLcetxU9uu",
-        },
-         {
-          type: "MES",
-          business: "鼎捷",
-          system: "K3C",
-          link: "https://kdocs.cn/l/cdisF3opU7TZ",
-          guide: "https://kdocs.cn/l/ciCKCt0LerK0",
-        },
-         {
-          type: "MES",
-          business: "鼎捷",
-          system: "K3Wise",
-          link: "https://kdocs.cn/l/ct0Td4RFj3kL",
-          guide: "https://kdocs.cn/l/crBz7ncHWGTB",
+          "type": "MES",
+          "business": "黑湖",
+          "system": "鼎捷E10",
+          "link": "https://www.kdocs.cn/l/ccl9N3TPnyOk"
         },
         {
-          type: "MES",
-          business: "甄一",
-          system: "SAP",
-          link: "https://kdocs.cn/l/caCpgmILVRsW",
-          guide: "https://kdocs.cn/l/cdehWkm6W4Eq",
+          "type": "MES",
+          "business": "语祯",
+          "system": "优时",
+          "link": "https://kdocs.cn/l/cl5aF6JjF0LS"
         },
         {
-          type: "MES",
-          business: "智引",
-          system: "U9",
-          link: "https://kdocs.cn/l/cqtYZyIz7dPw",
-          guide: "https://kdocs.cn/l/cpFSvlpSORKP",
+          "type": "MES",
+          "business": "语祯",
+          "system": "金蝶K3",
+          "link": "https://kdocs.cn/l/chn5qOhPu2by"
         },
         {
-          type: "MES",
-          business: "中科",
-          system: "U8",
-          link: "https://kdocs.cn/l/cbvMhiPrDJEU",
-          guide: "https://kdocs.cn/l/clmvWL9q4Dww",
+          "type": "MES",
+          "business": "语祯",
+          "system": "鼎捷易飞",
+          "link": "https://kdocs.cn/l/ceofBYQioRLZ"
         },
         {
-          type: "MES",
-          business: "树根互联",
-          system: "SAP B1 on MSSQL",
-          link: "https://kdocs.cn/l/co728nkzzvRB",
-          guide: "https://kdocs.cn/l/cdQc8NtpbVJd",
+          "type": "MES",
+          "business": "鼎捷",
+          "system": "用友NC",
+          "link": "https://kdocs.cn/l/cdmVewGi27d0"
         },
         {
-          type: "MES",
-          business: "万腾MES",
-          system: "U8",
-          link: "https://kdocs.cn/l/cnYZbuda6QFa",
-          guide: "https://kdocs.cn/l/cpPJAroyUMoW",
+          "type": "MES",
+          "business": "鼎捷",
+          "system": "金蝶K3C",
+          "link": "https://kdocs.cn/l/cdisF3opU7TZ"
         },
         {
-          type: "MES",
-          business: "赛瀚德",
-          system: "T+",
-          link: "https://kdocs.cn/l/cqa2yBVVAiNb",
-          guide: "https://kdocs.cn/l/cokSbbFGDHZ8",
+          "type": "MES",
+          "business": "鼎捷",
+          "system": "金蝶K3Wise",
+          "link": "https://kdocs.cn/l/ct0Td4RFj3kL"
         },
         {
-          type: "OA",
-          business: "泛微",
-          system: "SAP",
-          link: "https://www.kdocs.cn/l/coFuhJcQdrvD",
-          guide: "https://kdocs.cn/l/cj1gfJEx8as7",
+          "type": "MES",
+          "business": "甄一",
+          "system": "SAP",
+          "link": "https://kdocs.cn/l/caCpgmILVRsW"
         },
         {
-          type: "OA",
-          business: "泛微",
-          system: "K3C",
-          link: "https://kdocs.cn/l/cqCPBTmgwWmp",
-          guide: "https://kdocs.cn/l/cbLV5y5uhSlE",
+          "type": "MES",
+          "business": "智引",
+          "system": "用友U9",
+          "link": "https://kdocs.cn/l/cqtYZyIz7dPw"
         },
         {
-          type: "OA",
-          business: "泛微",
-          system: "甄云",
-          link: "https://www.kdocs.cn/l/cgbchnUzU3b6",
-          guide: "https://www.kdocs.cn/l/chX87BQzDlPm",
+          "type": "MES",
+          "business": "中科",
+          "system": "用友U8",
+          "link": "https://kdocs.cn/l/cbvMhiPrDJEU"
         },
         {
-          type: "CRM",
-          business: "纷享销客",
-          system: "K3C",
-          link: "https://www.kdocs.cn/l/cu052Bc7ZzLh",
-          guide: "https://kdocs.cn/l/ckPIFe8EuLgg",
+          "type": "MES",
+          "business": "树根互联",
+          "system": "SAP B1 on MSSQL",
+          "link": "https://kdocs.cn/l/co728nkzzvRB"
         },
         {
-          type: "PLM",
-          business: "数码大方",
-          system: "U8",
-          link: "https://kdocs.cn/l/cvW2V4maqjhj",
-          guide: "https://kdocs.cn/l/cqDNTmtaOUV8",
+          "type": "MES",
+          "business": "万腾",
+          "system": "用友U8",
+          "link": "https://kdocs.cn/l/cnYZbuda6QFa"
         },
         {
-          type: "PLM",
-          business: "数码大方",
-          system: "K3C",
-          link: "https://kdocs.cn/l/ca1atCwRPcHw",
-          guide: "https://kdocs.cn/l/cdbpzmC5gFjw",
+          "type": "MES",
+          "business": "博伊特",
+          "system": "浪潮",
+          "link": "https://www.kdocs.cn/l/ckvxqCsU1SCM"
         },
         {
-          type: "PLM",
-          business: "山东华天",
-          system: "SAP ECC",
-          link: "https://kdocs.cn/l/cp9TZObuYe5E",
-          guide: "https://kdocs.cn/l/cdyY2czu5P6x",
+          "type": "MES",
+          "business": "益模",
+          "system": "用友U8",
+          "link": "https://www.kdocs.cn/l/ct9VFij4J5uL"
         },
         {
-          type: "跨境电商",
-          business: "赛盒",
-          system: "T+",
-          link: "https://kdocs.cn/l/csfwIZzDqser",
-          guide: "https://kdocs.cn/l/cdkpCReL6JbK",
+          "type": "MES",
+          "business": "赛瀚德",
+          "system": "用友畅捷通T+",
+          "link": "https://kdocs.cn/l/cqa2yBVVAiNb"
         },
         {
-          type: "IoT",
-          business: "盛源成",
-          system: "SAP",
-          link: "https://kdocs.cn/l/clzJwEyaHOMd",
-          guide: "https://kdocs.cn/l/ciKnRp3wqT7C",
+          "type": "MES",
+          "business": "赛瀚德",
+          "system": "天心",
+          "link": "https://www.kdocs.cn/l/cdc3VyHTIy21"
         },
         {
-          type: "数字工厂",
-          business: "华为",
-          system: "优时",
-          link: "https://kdocs.cn/l/cl5aF6JjF0LS",
-          guide: "https://kdocs.cn/l/ccn03HQve8Em",
+          "type": "MES",
+          "business": "赛瀚德",
+          "system": "东信达",
+          "link": "https://www.kdocs.cn/l/corBnHEBmLeJ"
         },
         {
-          type: "BI",
-          business: "帆软",
-          system: "SAP",
-          link: "https://docs.qq.com/sheet/DVWNrVnpnRGxBcEdS?tab=BB08J2",
-          guide: "https://kdocs.cn/l/cqjKGQFGvNxE",
+          "type": "MES",
+          "business": "赛瀚德",
+          "system": "金蝶K3Wise",
+          "link": "https://kdocs.cn/l/csFnunTDTXAM"
         },
         {
-          type: "APPCube",
-          business: "华为",
-          system: "SAP",
-          link: "https://kdocs.cn/l/cqjvAcCgRAhG",
-          guide: "https://kdocs.cn/l/ciiZhpfLo9iC",
+          "type": "MES",
+          "business": "赛瀚德",
+          "system": "用友U9",
+          "link": "https://kdocs.cn/l/cq8pWVZWtI3d"
         },
+        {
+          "type": "OA",
+          "business": "泛微",
+          "system": "SAP",
+          "link": "https://www.kdocs.cn/l/coFuhJcQdrvD"
+        },
+        {
+          "type": "OA",
+          "business": "泛微",
+          "system": "SAP B1",
+          "link": "https://www.kdocs.cn/l/ccOVZj0M6ft6"
+        },
+        {
+          "type": "OA",
+          "business": "泛微",
+          "system": "用友NC",
+          "link": "https://docs.qq.com/sheet/DYkhLbmFESUFLR1pX?tab=pprqj1&_t=1673832900470"
+        },
+        {
+          "type": "OA",
+          "business": "泛微",
+          "system": "金蝶K3C",
+          "link": "https://kdocs.cn/l/cqCPBTmgwWmp"
+        },
+        {
+          "type": "OA",
+          "business": "泛微",
+          "system": "金蝶K3C",
+          "link": "https://www.kdocs.cn/l/cpGauqNkq6Ct"
+        },
+        {
+          "type": "OA",
+          "business": "泛微",
+          "system": "鼎捷E10",
+          "link": "https://docs.qq.com/sheet/DRWpIa0VXalpQRFFF?tab=fysejp"
+        },
+        {
+          "type": "OA",
+          "business": "泛微",
+          "system": "甄云SRM",
+          "link": "https://www.kdocs.cn/l/cgbchnUzU3b6"
+        },
+        {
+          "type": "OA",
+          "business": "致远",
+          "system": "用友NC",
+          "link": "https://kdocs.cn/l/clQpFwe7L8u2"
+        },
+        {
+          "type": "OA",
+          "business": "致远",
+          "system": "用友U9",
+          "link": "https://www.kdocs.cn/l/cc74nOKpTrR0"
+        },
+        {
+          "type": "OA",
+          "business": "蓝凌",
+          "system": "鼎捷E10",
+          "link": "https://www.kdocs.cn/l/csDva11xxDeM"
+        },
+        {
+          "type": "慧通差旅",
+          "business": "华为",
+          "system": "企业微信",
+          "link": "https://www.kdocs.cn/l/cvWDAZ2CRsw7"
+        },
+        {
+          "type": "慧通差旅",
+          "business": "华为",
+          "system": "飞书",
+          "link": "https://www.kdocs.cn/l/cbhk21aql3QJ"
+        },
+        {
+          "type": "慧通差旅",
+          "business": "华为",
+          "system": "红帆",
+          "link": "https://www.kdocs.cn/l/camArbtLfomz"
+        },
+        {
+          "type": "慧通差旅",
+          "business": "华为",
+          "system": "鼎捷E10",
+          "link": "https://kdocs.cn/l/cs0Eh4pD18Qu"
+        },
+        {
+          "type": "APPCube",
+          "business": "华为",
+          "system": "SAP",
+          "link": "https://kdocs.cn/l/cqjvAcCgRAhG"
+        },
+        {
+          "type": "工单宝",
+          "business": "华为",
+          "system": "金蝶K3C",
+          "link": "https://www.kdocs.cn/l/csN5ACl1JYK9"
+        },
+        {
+          "type": "CRM",
+          "business": "纷享销客",
+          "system": "金蝶K3C",
+          "link": "https://www.kdocs.cn/l/csN5ACl1JYK9"
+        },
+        {
+          "type": "CRM",
+          "business": "纷享销客",
+          "system": "用友NC",
+          "link": "https://docs.qq.com/sheet/DYklleWJqTWVUeVdw?tab=pc4kov"
+        },
+        {
+          "type": "PLM",
+          "business": "数码大方",
+          "system": "用友U8",
+          "link": "https://kdocs.cn/l/cvW2V4maqjhj"
+        },
+        {
+          "type": "PLM",
+          "business": "数码大方",
+          "system": "金蝶K3C",
+          "link": "https://kdocs.cn/l/ca1atCwRPcHw"
+        },
+        {
+          "type": "PLM",
+          "business": "山东华天",
+          "system": "SAP ECC",
+          "link": "https://kdocs.cn/l/cp9TZObuYe5E"
+        },
+        {
+          "type": "跨境电商",
+          "business": "赛盒",
+          "system": "用友畅捷通T+",
+          "link": "https://kdocs.cn/l/csfwIZzDqser"
+        },
+        {
+          "type": "IoT",
+          "business": "盛源成",
+          "system": "SAP",
+          "link": "https://kdocs.cn/l/clzJwEyaHOMd"
+        },
+        {
+          "type": "IoT",
+          "business": "工业物联",
+          "system": "Kafka",
+          "link": "https://www.kdocs.cn/l/cqN2LF94WwuJ"
+        },
+        {
+          "type": "数字工厂",
+          "business": "华为",
+          "system": "优时",
+          "link": "https://kdocs.cn/l/cl5aF6JjF0LS"
+        },
+        {
+          "type": "BI",
+          "business": "帆软",
+          "system": "SAP",
+          "link": "https://docs.qq.com/sheet/DVWNrVnpnRGxBcEdS?tab=BB08J2"
+        },
+        {
+          "type": "财税",
+          "business": "标普云",
+          "system": "鼎捷E10",
+          "link": "https://www.kdocs.cn/l/cs17y0UKsBBb"
+        }
       ],
       arr1: [],
       arr2: [],
@@ -295,7 +414,7 @@ export default {
         let key = item.type + "|" + item.business;
         obj[key] = obj[key] || []
         obj[key].push(idx);
-        
+
         obj1[item.type] = obj1[item.type] || [];
         obj1[item.type].push(idx);
       });
